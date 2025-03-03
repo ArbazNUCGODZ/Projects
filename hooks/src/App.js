c73a4ad5fa761264a995fd5aa9a1749aa0a14e20
ArbazNUCGODZ <ak8411831777@gmail.com>
2025-03-03 14:42:32 +0530

    hello

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,16 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Ex1 from './hooks/Ex1';
-import Ex2 from './hooks/Ex2';
-import Ex3 from './hooks/Ex3';
+// import Ex1 from './hooks/Ex1';
+// import Ex2 from './hooks/Ex2';
+// import Ex3 from './hooks/Ex3';
+// import Prop from './hooks/Prop';
+// import Childprop from './hooks/Childprop';
+// import Counter from './hooks/Counter';
+// import Usecallback from './hooks/Usecallback';
+// import Usememo from './hooks/Usememo';
+import UseFetch from './hooks/UseFetch';
+import ThemeContext from './hooks/ThemeContext';
+import Themetoggle from './hooks/Themetoggle';
+
 
 function App() {
   return (
-    <><Ex1/>
-    
-    <Ex2/>
-    <Ex3/></>
+
+    <>
+   <ThemeContext>
+    <Themetoggle/>
+
+   </ThemeContext>
+
+    {/* <Counter/> */}
+    {/* <Usecallback/> */}
+    {/* <Usememo/> */}
+    {/* <UseFetch/> */}
+    </>
   );
 }
 
 export default App;
+
+
+  {/* {/* <Prop>
+      <Childprop/>
+      </Prop>    </> */}
+    // <><Ex1/>
+    
+    // <Ex2/>
+    // <Ex3/></> */}
